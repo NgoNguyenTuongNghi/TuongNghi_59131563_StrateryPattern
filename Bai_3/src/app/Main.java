94020329fdd8f0076e1caced9b6d367eb1d47ace
Tường Nghi <62927599+NgoNguyenTuongNghi@users.noreply.github.com>
2020-04-18 11:31:17 +0700

    v1.3

--- Bai_3/src/app/Main.java
+++ Bai_3/src/app/Main.java
@@ -1,16 +1,19 @@
 package app;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Main {
     public static void main(final String[] args) throws Exception {
         final SoSanhTheoDiem soSanhTheoDiem = new SoSanhTheoDiem();
         final SoSanhTheoTen soSanhTheoTen = new SoSanhTheoTen();
 
-        SinhVien sv1 = new SinhVien("Ngo Nguyen Tuong Nghi", Date("1995/05/19"), 8.8f);
-        SinhVien sv2 = new SinhVien("Diep Tu Dat", Date("1999/09/19"), 9.9f);
-        SinhVien sv3 = new SinhVien("Tran Thanh Thi", Date("1998/08/19"), 10.0f);
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+        SinhVien sv1 = new SinhVien("Ngo Nguyen Tuong Nghi", df.parse("19/05/1995"), 8.8f);
+        SinhVien sv2 = new SinhVien("Diep Tu Dat", df.parse("19/09/1999"), 9.9f);
+        SinhVien sv3 = new SinhVien("Tran Thanh Thi", df.parse("19/08/1998"), 10.0f);
 
         ArrayList<SinhVien> sv = new ArrayList<>();
         sv.add(sv1);
@@ -21,7 +24,7 @@ public class Main {
         qlsv.setDs(sv);
         qlsv.setiSoSanh(soSanhTheoTen);
         qlsv.sapXep();
-        System.out.println("Danh sach sap xep theo ho ten: ");
+        System.out.println("\nDanh sach sap xep theo ho ten: ");
         qlsv.inDS();
 
         qlsv.setiSoSanh(soSanhTheoDiem);
@@ -29,10 +32,6 @@ public class Main {
         System.out.println("\nDanh sach sap xep theo diem: ");
         qlsv.inDS();
 
-    }
-
-    private static Date Date(String string) {
-        return null;
     }
 
 }
--- Bai_3/src/app/SoSanhTheoDiem.java
+++ Bai_3/src/app/SoSanhTheoDiem.java
@@ -3,7 +3,7 @@ package app;
 public class SoSanhTheoDiem implements ISoSanh<SinhVien> {
     @Override
     public int soSanh(SinhVien o1, SinhVien o2) {
-        if(o1.getDiemTB() > o2.getDiemTB()) 
+        if(o1.getDiemTB() < o2.getDiemTB()) 
             return 1;
         else 
             if(o1.getDiemTB() == o2.getDiemTB()) 
